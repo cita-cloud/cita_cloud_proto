274ebf4c9a7146d6da9ddab19e4e1d11618f6080
rink1969 <rink1969@gmail.com>
2022-10-27 05:52:17 +0000

    update deps

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+// tonic does not derive `Eq` for the gRPC message types, which causes a warning from Clippy. The
+// current suggestion is to explicitly allow the lint in the module that imports the protos.
+// Read more: https://github.com/hyperium/tonic/issues/1056
+#![allow(clippy::derive_partial_eq_without_eq)]
+
 pub mod blockchain {
     tonic::include_proto!("blockchain");
 }
